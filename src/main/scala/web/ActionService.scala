7dec395a3a912890903f795bcef3673a4e26d1a6
Daneil Blyth <rdblyth@gmail.com>
2016-08-09 21:48:18 -0400

    Simplified the conversion of Action to and from Json by writing converters for the Verb and ObjectType and using the built in json converstion for the case class

--- src/main/scala/web/ActionService.scala
+++ src/main/scala/web/ActionService.scala
@@ -17,21 +17,17 @@ import scala.concurrent.ExecutionContextExecutor
 import scala.util.{Failure, Success}
 
 trait Protocols extends DefaultJsonProtocol {
-  implicit object VerbJsonFormat extends RootJsonFormat[Action] {
-    def write(a: Action) = JsObject(
-      "id" -> JsNumber(a.id.get),
-      "verb" -> JsString(a.verb),
-      "objectType" -> JsString(a.objectType),
-      "objectUri" -> JsString(a.objectUri))
-
-    def read(value: JsValue) = {
-      value.asJsObject.getFields("id", "verb", "objectType", "objectUri") match {
-        case Seq(JsNumber(id), JsString(verb), JsString(objectType), JsString(objectUri)) =>
-          Action(Some(id.toInt), verb, objectType, objectUri)
-        case _ => throw new DeserializationException("Action expected")
-      }
-    }
+  implicit object VerbJsonFormat extends RootJsonFormat[Verb] {
+    def write(v: Verb) = JsString(v)
+    def read(json: JsValue) : Verb =  Verb.stringToVerb(json.compactPrint)
   }
+
+  implicit object ObjectTypeJsonFormat extends RootJsonFormat[ObjectType] {
+    def write(o: ObjectType) = JsString(o)
+    def read(json: JsValue) : ObjectType =  ObjectType.stringToObjectType(json.compactPrint)
+  }
+
+  implicit val actionJsonFormat = jsonFormat4(Action)
 }
 
 trait Service extends Protocols with DbConfiguration {
