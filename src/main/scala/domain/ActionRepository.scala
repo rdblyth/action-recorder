fa60e4880cae787b489d78e9b2d9c2e8ac442b47
Daneil Blyth <rdblyth@gmail.com>
2016-08-09 21:36:44 -0400

    Moved duplicate code to a private method

--- src/main/scala/domain/ActionRepository.scala
+++ src/main/scala/domain/ActionRepository.scala
@@ -14,10 +14,12 @@ class ActionsRepository(val dbConfig: DatabaseConfig[JdbcProfile])
     .run(actions returning actions.map(_.id) += action)
     .map(id => action.copy(id = Some(id)))
 
-  def delete(id: Int) = db.run(actions.filter(_.id === id).delete) map { _ > 0 }
+  def delete(id: Int) = db.run(filterById(id).delete) map { _ > 0 }
 
-  def findById(id: Int) = db.run(actions.filter(_.id === id).result.headOption)
+  def findById(id: Int) = db.run(filterById(id).result.headOption)
 
   def init() = db.run(DBIOAction.seq(actions.schema.create))
   def drop() = db.run(DBIOAction.seq(actions.schema.drop))
+
+  private def filterById(id: Int) = actions.filter(_.id === id)
 }
